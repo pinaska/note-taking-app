efe7200884629dac9f9fbc792660a69d74fabe95
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-08-13 14:37:03 -0700

    add READEME, add TODO comments in js

---
+++ README.md
@@ -0,0 +1,9 @@
+** Notes creating app with JS only **
+I wanted to learn bulma css framework to compare it with Bootstrap, and to create the app, using plain JavaScript.
+I build similar functionality on TO DO app, but it was with jQuery [link to to-do project](https://pinaska.github.io/to-do/)
+
+*** Built log ***
+08.13 - started with function addNewNote
+
+-----------------------
+:computer: [check out all my projects](https://github.com/pinaska)
--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.querySelector('.add-new-note').addEventListener('click', function(e){
     addNewNote();
 });
 
+//TODO: 1. check if textarea.lenght !=0; 2. add keyup for create new note 
 function addNewNote(){
     //get the placeholder tekst into note
     var getText = document.querySelector('.textarea').value;
@@ -17,9 +18,11 @@ function addNewNote(){
     //add the class of bulma column to each  note
     newNote.classList.add('column');
     newNote.appendChild(newNoteText);
-
-
     // console.log('You have created a new note with the text: ' + getNewNoteText); 
     //append the new note to the div
     document.querySelector('.columns').appendChild(newNote);
 }
+
+//TODO 3. function deleteNote + button to delete it
+//TODO 4. function modifyNote + button to modify it
+//TODO 5. function pickNoteColor (pick from 4 colours)

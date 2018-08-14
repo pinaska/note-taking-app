fac85e1f28369b35f1016587da57f34715762fc0
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-08-14 06:03:06 -0700

    add if to prevent note creation when there is no text typed

--- script.js
+++ script.js
@@ -5,26 +5,60 @@ document.querySelector('.add-new-note').addEventListener('click', function(e){
     addNewNote();
 });
 
-//TODO: 1. check if textarea.lenght !=0; 2. add keyup for create new note 
 function addNewNote(){
     //get the placeholder tekst into note
     var getText = document.querySelector('.textarea').value;
     //reset the text area
     document.querySelector('.textarea').value = '';
 
-    var newNoteText = document.createTextNode(getText);
-    //create the node = div
-    var newNote = document.createElement('DIV');
-    //add the class of bulma column to each  note
-    newNote.classList.add("column", "box", "is-one-quarter");
-    // newNote.classList.add("is-child");
-    // newNote.classList.add("box");
-    newNote.appendChild(newNoteText);
-    // console.log('You have created a new note with the text: ' + getNewNoteText); 
-    //append the new note to the div
-    document.querySelector('#note-item').appendChild(newNote);
+    if(getText.length != 0){
+        //create the note
+        var newNoteText = document.createTextNode(getText);
+        var newNote = document.createElement('DIV');
+
+        //add the class of bulma column to each  note
+        newNote.classList.add("column", "box", "is-one-quarter", "newNote");
+        newNote.appendChild(newNoteText);
+
+        //create button to change background color and append it to the note. Need to think if I will use it.
+        var btn = document.createElement("BUTTON");
+        var t = document.createTextNode("modify note");
+        btn.classList.add("button");
+        btn.appendChild(t);
+
+        //append the new note to the div
+        document
+            .querySelector("#note-item")
+            .appendChild(newNote)
+            .appendChild(btn);
+
+    }
+    else{
+        alert('you did not put any note text. Please type into the text box.');
+    }
+
 }
+
 
 //TODO 3. function deleteNote + button to delete it
 //TODO 4. function modifyNote + button to modify it
+
+
+
+
 //TODO 5. function pickNoteColor (pick from 4 colours)
+//the color picker - pink
+var pickColorPink = document.createElement("DIV");
+pickColorPink.setAttribute("id", "pickPink");
+pickColorPink.style.width = "24px";
+pickColorPink.style.height = "24px";
+pickColorPink.style.backgroundColor = "#8A4D76";
+
+    // var pickColorYellow = document.createElement("DIV");
+    // pickColorYellow.style.width = "24px";
+    // pickColorYellow.style.height = "24px";
+    // pickColorYellow.style.backgroundColor = "#ffdd57";
+
+    // var pickColorPinkFirst = document.getElementById("pickPink");
+    // var colorPicker = pickColorPinkFirst.parentNode;
+    // colorPicker.insertBefore(pickColorYellow, pickColorPinkFirst);
